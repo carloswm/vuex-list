453b50635e68c46be283ac80ca7bc1c6ab86a77e
Carlos Marigorda <carlos.wmarigorda@gmail.com>
2020-09-10 02:41:41 -0500

    git init

---
+++ src/components/List.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <ul class="list-group">
+      <li  v-for="(item, index) of arrayOrdenado" :key="item" @click="aumentar(index)"
+      class="list-group-item d-flex justify-content-between align-items-center">
+        {{item.nombre}}
+        <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+      </li>
+    </ul>
+    <button type="button" class="btn btn-danger btn-lg btn-block" @click="reset">Reiniciar</button> 
+  </div>
+</template>
+
+<script>
+
+import {mapState, mapMutations} from 'vuex';
+
+export default {
+    name: 'List',
+    computed: {
+      ...mapState(['frutas']),
+      arrayOrdenado(state) {
+        let orden = (a,b) => b.cantidad - a.cantidad;
+        return state.frutas.sort(orden);
+      }
+    },
+    methods: {
+      ...mapMutations(['aumentar','reset'])
+    }
+}
+</script>
